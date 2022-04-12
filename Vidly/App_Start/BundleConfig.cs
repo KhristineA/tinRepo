607da07ac69b7f0530d23e78c1b41ab8dce09d7a
khristine <khristine.arante@gmail.com>
2022-04-12 11:33:13 +0800

    add theme, add cancel button, add username logged in in home page

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -27,6 +27,7 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap-theme2.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
                       "~/Content/toastr.css",
