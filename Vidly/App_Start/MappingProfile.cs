6f2bef933a977eb6dcf202344c22c63bb19df98b
khristine <khristine.arante@gmail.com>
2022-04-16 22:19:49 +0800

    Rental page - create index and server-side pagination and filter

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -16,13 +16,16 @@ namespace Vidly.App_Start
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<Movie, MovieDto>();
             Mapper.CreateMap<MembershipType, MembershipTypeDto>();
-            Mapper.CreateMap<Genre, GenreDto > ();
+            Mapper.CreateMap<Genre, GenreDto >();
+            Mapper.CreateMap<Rental, RentalDto>();
 
             //Dto to Domain
             Mapper.CreateMap<CustomerDto, Customer>()
                 .ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<MovieDto, Movie>()
                 .ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<RentalDto, Rental>()
+                .ForMember(r => r.Id, opt => opt.Ignore());
         }
     }
 }
--- Vidly/Controllers/RentalsController.cs
+++ Vidly/Controllers/RentalsController.cs
@@ -1,18 +1,84 @@
-﻿using System;
+﻿using DataTables.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class RentalsController : Controller
     {
+
+        public ApplicationDbContext _context;
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult New()
         {
             return View();
         }
 
+        public ActionResult Index()
+        {
+            return View();
+        }
 
+        public ActionResult Get([ModelBinder(typeof(DataTablesBinder))] IDataTablesRequest requestModel)
+        {
+            var rentalQuery = _context.Rentals
+                .Include(r => r.Customer)
+                .Include(r => r.Movie);
+
+            var totalCount = rentalQuery.Count();
+
+            #region Filtering
+            // Apply filters for searching
+            if (requestModel.Search.Value != string.Empty)
+            {
+                var value = requestModel.Search.Value.Trim();
+                rentalQuery = rentalQuery.Where(r => r.Customer.Name.Contains(value));
+            }
+
+            var filteredCount = rentalQuery.Count();
+
+            #endregion Filtering
+
+            #region Sorting
+            // Sorting
+            //var sortedColumns = requestModel.Columns.GetSortedColumns();
+            //var orderByString = String.Empty;
+
+            //foreach (var column in sortedColumns)
+            //{
+            //    orderByString += orderByString != String.Empty ? "," : "";
+            //    orderByString += (column.Data) +
+            //      (column.SortDirection ==
+            //      Column.OrderDirection.Ascendant ? " asc" : " desc");
+            //}
+
+            rentalQuery = rentalQuery.OrderBy(r => r.Customer.Name);
+
+            #endregion Sorting
+
+            // Paging
+            rentalQuery = rentalQuery.Skip(requestModel.Start).Take(requestModel.Length);
+
+            var data = rentalQuery.Select(rental => new
+            {
+                Customer_Id = rental.Customer.Name,
+                Movie_Id = rental.Movie.Name,
+                Id = rental.Id
+            }).ToList();
+
+            return Json(new DataTablesResponse
+            (requestModel.Draw, data, filteredCount, totalCount),
+                        JsonRequestBehavior.AllowGet);
+        }
     }
 }
